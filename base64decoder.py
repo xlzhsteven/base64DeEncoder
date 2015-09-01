ce2af40aaebce1a754dda8b364adabdedb815838
Xiaolong Zhang <xlzhsteven@gmail.com>
2015-08-31 22:37:46 -0700

    add arg for file name and option to decode or encode

--- base64decoder.py
+++ base64decoder.py
@@ -2,12 +2,16 @@
 import base64
 import argparse
 
-parser = argparse.ArgumentParser(description='Enter CALResponse file name.')
+parser = argparse.ArgumentParser(description='Enter CAL Response file name.')
 parser.add_argument("fileName")
+parser.add_argument("option")
 args = parser.parse_args()
 with open(args.fileName) as f:
 	output = open('output.txt', 'w')
 	content = f.readlines()
 	for i in range(len(content)):
-		output.write(base64.decodestring(content[i]))
+		if args.option.lower() == 'encode':
+			output.write(base64.encodestring(content[i]))
+		else:
+			output.write(base64.decodestring(content[i]))
 	output.close()
